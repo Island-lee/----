432a258268d4b7695412b079544baf2fa77b335e
Island-lee <79835642+Island-lee@users.noreply.github.com>
2022-05-05 15:42:03 +0800

    Update neurl network.py

--- neurl network.py
+++ neurl network.py
@@ -1,99 +1,14 @@
-# from pyhanlp import *
-# import hanlp
-# # 通过 SafeJClass 取得 HanLP 中的 CorpusLoader 类
-# CorpusLoader = SafeJClass('com.hankcs.hanlp.corpus.document.CorpusLoader')
-# sents = CorpusLoader.convert2SentenceList(r'C:\Users\lab702\Desktop\1.txt')
-# for sent in sents:
-#     print(sent)
-
-# from pyhanlp import *
-# from test_utility import ensure_data
-#
-# PerceptronNameGenderClassifier = JClass('com.hankcs.hanlp.model.perceptron.PerceptronNameGenderClassifier')
-# cnname = ensure_data('cnname', 'http://file.hankcs.com/corpus/cnname.zip')
-# TRAINING_SET = os.path.join(cnname, 'train.csv')
-# TESTING_SET = os.path.join(cnname, 'test.csv')
-# MODEL = cnname + ".bin"
-
-# def run_classifier(averaged_perceptron):
-#     print('=====%s=====' % ('平均感知机算法' if averaged_perceptron else '朴素感知机算法'))
-#     classifier = PerceptronNameGenderClassifier()
-#     print('训练集准确率：', classifier.train(TRAINING_SET, 10, averaged_perceptron))
-#     model = classifier.getModel()
-#     print('特征数量：', len(model.parameter))
-#     # model.save(MODEL, model.featureMap.entrySet(), 0, True)
-#     # classifier = PerceptronNameGenderClassifier(MODEL)
-#     for name in "赵建军", "沈雁冰", "陆雪琪", "李冰冰":
-#         print('%s=%s' % (name, classifier.predict(name)))
-#     print('测试集准确率：', classifier.evaluate(TESTING_SET))
-
-# if __name__ == '__main__':
-#     run_classifier(False)
-#     run_classifier(True)
-
-
-
 import pandas as pd
 test =pd.read_csv(r'D:\anaconda3\envs\newpytorch1\Lib\site-packages\pyhanlp\static\data\test\cnname\train.csv',header=None)
-# print(test.iloc[[0]][1])
-# print(test.head())
-# for i,line in test.iterrows():
-#     if i==10:
-#         break
-#     print(i,line)
-
-# with open('labelList1.csv', 'w') as f:
-#     for i,line in test.iterrows():
-#         # if i == 1000:
-#         #     break
-#         if line[1]=='男':
-#             f.write('0 1')
-#             f.write('\n')
-#         else:
-#             f.write('1 0')
-#             f.write('\n')
-
 name = ""
 for i, line in test.iterrows():
     name += line[0][1:]
 a = set(list(name+'_'))
 aa = list(a)
+
 # #保存词典
 with open('NameList1.txt','w') as f:
     f.writelines(aa)
-
-# 读取词典
-# with open('NameList1.txt','r') as f:
-#     for t in f:
-#         print(t)
-#         print(len(t))
-
-#编码
-# with open('NameList1.txt','r') as f:
-#     for t in f:
-#         with open('train1.csv','w')as ft:
-#             for i,line in test.iterrows():
-#                 if i==1000:
-#                     break
-#                 temp = [0]*len(t)
-#                 if len(line[0])==3:
-#                     index1 =t.index(line[0][1])
-#                     index2 =t.index(line[0][2])
-#                     temp[index1]=1
-#                     temp[index2]=1
-#                     # print(str(temp))
-#                     # print(str(temp)[1:-1])
-#                     ft.write(str(temp)[1:-1]+'\n')
-#                 if len(line[0])==2:
-#                     index1 = t.index(line[0][1])
-#                     index2 = t.index('_')
-#                     temp[index1]=1
-#                     temp[index2]=1
-#                     ft.write(str(temp)[1:-1]+'\n')
-
-# with open('train1.csv','r') as f:
-    # for t in f:
-        # print(t)
 
 #建立网络
 import torch
@@ -119,11 +34,6 @@ class Perceptron(nn.Module):
         x = self.linear(x)
         output = self.linear2(x)
         return output
-# train_data = pd.read_csv('train1.csv',header=None)
-# train_label =pd.read_csv('labelList2.csv',header=None)
-
-# test_data =1
-# test_label =1
 
 def img_transform(img, label):
     label = numpy.array(label)
@@ -181,10 +91,9 @@ class Name(Dataset):
         return int(len(self.data))
 
 train_data = Name(True,img_transform)
-# test_data = Name(False,img_transform)
+
 BATCH_SIZE = 100
 train_data = DataLoader(train_data, batch_size=BATCH_SIZE, shuffle=True, num_workers=1)
-# val_data = DataLoader(val_data, batch_size=BATCH_SIZE, shuffle=False, num_workers=1)
 
 def main():
     net = Perceptron()
